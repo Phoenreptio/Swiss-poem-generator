55613155103e9dad027264631320380db535caf9
Phoenreptio <serpieri23@gmail.com>
2025-10-05 15:28:52 +0200

    added hints, improved CSS

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ event.preventDefault();
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey ="f6fc8d6e97o4b220033t044a1041dfbf";
 let prompt = `User's instructions: Generate a short poem about ${instructionsInput.value}`;
-let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a Swiss 4 line poem in basic HTML and separate each line with a <br />. Please follow the user's instructions and sign with 'Shecodes AI' at the end of the poem";
+let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a Swiss 4 line poem and separate each line with a <br />. Please follow the user's instructions and sign with 'Shecodes AI' at the end of the poem";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+let poemElement=document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML =`Generating poem about ${instructionsInput.value}`;
 
 console.log("Generating poem");
 console.log(`Prompt: ${prompt}`);
